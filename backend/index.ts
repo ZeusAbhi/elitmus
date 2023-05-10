0b87f00fa21cf653cb1d01aa31cc0c4709552601
lohit244 <lohit244@gmail.com>
2023-05-10 10:42:52 +0530

    feat: db schemas ( probably not final )

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,21 @@
-console.log("hello")
+import { Prisma, PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+const leaderboard = prisma.user.aggregate({
+  select: {
+    userId: { _count: true },
+    totalTime: {
+      sum: {
+        endTime: {
+          minus: "startTime"
+        }
+      }
+    }
+  },
+  orderBy: {
+    totalTime: "asc"
+  }
+});
+
+console.log(leaderboard);
