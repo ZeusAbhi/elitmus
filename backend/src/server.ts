5f9abaaaab34de9f37649458cdb3e35371a936db
lohit244 <lohit244@gmail.com>
2023-05-10 13:21:55 +0530

    chore: cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,10 @@
 import { Prisma, PrismaClient } from "@prisma/client"
+import cors from "cors"
 import express from "express"
 
 const prisma = new PrismaClient()
 const app = express();
+app.use(cors())
 app.use(express.json())
 
 export const PORT = process.env.PORT || 8080
