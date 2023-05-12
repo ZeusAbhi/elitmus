d40edc4650e46bd55bb7c5b15355ec75786c924a
lohit244 <lohit244@gmail.com>
2023-05-12 19:39:16 +0530

    feat: change color scheme

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 export default function Header() {
   const { user, logout } = useAuth()
   return (
-    <header className="flex flex-row md:px-4 px-2 bg-blue-100/50 text-blue-800 py-3 border-b border-blue-300">
+    <header className="flex flex-row md:px-4 px-2 bg-blue-800 text-white py-3 m-2 rounded-md shadow-md shadow-gray-400">
       <div className="flex flex-1 items-center">
         <Link href="/">
           <h1 className="text-xl font-semibold">Puzzle Hunt</h1>
@@ -14,11 +14,17 @@ export default function Header() {
       <div className="flex flex-row gap-2 flex-grow-0 items-center">
         {user ? (
           <>
-            <div className="px-2 py-2 bg-blue-200 rounded-md">{user.username}</div>
+            <Link href="/dashboard">
+              <div className="px-2 py-2 bg-blue-900/40 shadow-sm shadow-blue-900 rounded-md border-blue-900 border">{user.username}'s Dashboard</div>
+            </Link>
             <div className="cursor-pointer font-semibold" onClick={logout}>Logout</div>
           </>
         ) : (
-          <Link href="/login">Login</Link>
+          <Link href="/login">
+            <div className="px-2 py-2 bg-blue-900/40 shadow-sm shadow-blue-900 rounded-md border-blue-900 border">
+              Login
+            </div>
+          </Link>
         )}
       </div>
 
