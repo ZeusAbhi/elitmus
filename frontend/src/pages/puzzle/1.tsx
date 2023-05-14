0fe59433bbb093fe2f26a7e9a895d2beec4edf81
lohit244 <lohit244@gmail.com>
2023-05-15 01:49:03 +0530

    fix: unescaped entities

--- frontend/src/pages/puzzle/1.tsx
+++ frontend/src/pages/puzzle/1.tsx
@@ -8,7 +8,7 @@ export default function PuzzleOne() {
       <br />
       Finally, welcome to the company! I hope you enjoy the work you do here. Remember to always be on time, and to be
       respectful to your coworkers. Sometimes, you may have to work overtime, but that&apos;s okay!, Together, we can make this.
-      Don&apos;t forget to take breaks and have fun! As you can see, the company is growing quickly, and we need to hire more people to keep up with the demand. You'll
+      Don&apos;t forget to take breaks and have fun! As you can see, the company is growing quickly, and we need to hire more people to keep up with the demand. You&apos;ll
       be tasked with a lot of things, but you&apos;ll do great!
       <br />
       <br />
