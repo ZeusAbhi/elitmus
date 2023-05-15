470de63e555a9f18ffc1553b691c3bda343c0db7
lohit244 <lohit244@gmail.com>
2023-05-15 12:25:03 +0530

    fix: unescaped entities

--- frontend/src/pages/puzzle/4.tsx
+++ frontend/src/pages/puzzle/4.tsx
@@ -4,9 +4,9 @@ import PuzzleContainer from "@/components/PuzzleContainer"
 export default function PuzzleOne() {
   return <PuzzleContainer puzzleNum={4}>
     <div className="font-mono">
-      You arrive early to the meeting room, but as you're setting the computer in the meeting room, you realize that you can't remember the password to log in.
+      You arrive early to the meeting room, but as you&apos;re setting the computer in the meeting room, you realize that you can&apos;t remember the password to log in.
       <br />
-      No need to worry, its the CEO's first name, without any spaces or special characters.
+      No need to worry, its the CEO&apos;s first name, without any spaces or special characters.
       <br />
       <br />
       Can you decipher the password and successfully log in before the meeting starts? Time is running out!
